8f1d4b0f3f8b5ccc54caa6602df54a93dca81867
davidjimenez75 <davidjimenez75@gmail.com>
2025-06-08 11:56:17 +0200

    CSS - Moved to root folder as index.css

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
-    <link href="./css/starter-template.css" rel="stylesheet">
+    <link href="./index.css" rel="stylesheet">
 
     <!-- Vue.js cloak styles to prevent flash of unstyled content -->
     <style>
